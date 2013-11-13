eb9e758117e5cc64af1308dbbbc7cb13f8a2d50c
Ivo ánchez Checa Crosato <ivoscc@gmail.com>
2013-11-13 16:13:55 -0500

    Adding command to load all countries

---
+++ countrymodel/management/commands/load_all_countries.py
@@ -0,0 +1,18 @@
+#-*- coding:utf-8 -*-
+
+from  django.core.management.base import BaseCommand
+
+from countrymodel import MultiCountry
+
+from django_countries.countries import COUNTRIES
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        """
+        Loads all countries from django_countries as a record on the database.
+        """
+        for country in COUNTRIES:
+            c = MultiCountry(country=country[0])
+            c.save()
